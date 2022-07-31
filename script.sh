51741ba7bfa161253a7887927fcfd1f1a6dcea2c
Robert Polanski <robert.polanski@instersoft.de>
2022-07-31 14:45:49 +0200

    flux

---
+++ script.sh
@@ -0,0 +1,20 @@
+flux bootstrap github \
+  --owner=$GITHUB_USER \
+  --repository=mssandbox-gitops \
+  --branch=main \
+  --path=./root \
+  --personal
+
+flux create source git mssandbox \
+  --url=https://github.com/RobertPolanski/mssandbox-gitops \
+  --branch=master \
+  --interval=30s \
+  --export > ./root/mssandbox-source.yaml
+
+flux create kustomization mssandbox \
+  --target-namespace=mssandbox \
+  --source=mssandbox-gitops \
+  --path="./app" \
+  --prune=true \
+  --interval=1m \
+  --export > ./root/mssandbox-kustomization.yaml
