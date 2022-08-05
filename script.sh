90809ee4a944654f1bb06d8058d369050e4b998d
Robert Polanski <robert.polanski@instersoft.de>
2022-08-05 19:04:13 +0200

    more replica for grafana

--- script.sh
+++ script.sh
@@ -18,3 +18,19 @@ flux create kustomization mssandbox \
   --prune=true \
   --interval=1m \
   --export > ./root/mssandbox-kustomization.yaml
+
+
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add stable https://charts.helm.sh/stable
+helm repo update
+
+helm install --namespace monitoring prometheus prometheus-community/prometheus --set nodeExporter.hostRootfs=false
+helm delete prometheus -n monitoring
+
+kubectl expose service prometheus-server --type=NodePort --target-port=9090 --name=prometheus-server-ext -n monitoring
+
+helm repo add grafana https://grafana.github.io/helm-charts
+
+helm repo update
+
+helm install grafana bitnami/grafana -n monitoring
